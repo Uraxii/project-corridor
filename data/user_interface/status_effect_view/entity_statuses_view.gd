ff48d72b788a22b418b201fd57dd7c5fd1f99773
uraxii <accounts@brandonpaul.me>
2025-04-02 21:45:17 -0400

    status effect progress

--- data/user_interface/status_effect_view/entity_statuses_view.gd
+++ data/user_interface/status_effect_view/entity_statuses_view.gd
@@ -2,27 +2,30 @@ class_name EntityStatusesView extends HBoxContainer
 
 # TODO: Optimize this script.
 
+@onready var status_effect_view := preload("res://data/user_interface/status_effect_view/status_effect_view.tscn")
+
 var target:             Entity = null
-var tracked_effects:    Array[TextureRect] = []
+var effect_views:       Array[StatusEffectView] = []
 
 
 func set_target(entity: Entity) -> void:
         target = entity
+        clear_view()
 
 
 func clear_view() -> void:
-        for effect in tracked_effects:
+        for effect in effect_views:
                 effect.queue_free.call_deferred()
 
-        tracked_effects = []
+        effect_views = []
 
 
 func update_view() -> void:
         for effect in target.status_effects:
-                var icon = TextureRect.new()
-                icon.texture = effect.icon
-                tracked_effects.append(icon)
-                add_child(icon)
+                var effect_view: StatusEffectView = status_effect_view.instantiate()
+                add_child(effect_view)
+                effect_view.initialize(effect)
+                effect_views.append(effect_view)
 
 
 func _process(delta: float) -> void:
---
+++ data/user_interface/status_effect_view/status_effect_view.gd
@@ -0,0 +1,27 @@
+class_name StatusEffectView extends Node
+
+@onready var progress_bar = $ProgressBar
+@onready var icon: TextureRect = $Icon
+@onready var remaining_time = $RemainingTime
+@onready var timer = $Timer
+
+var skill: Skill = null
+
+
+func initialize(skill: Skill) -> void:
+        self.skill = skill
+        icon.texture = skill.icon
+
+        if skill.effect_duration == 0:
+                return
+
+        timer.wait_time = skill.effect_remaining_time
+        timer.start()
+
+        remaining_time.text = '%3.1f' % timer.time_left
+        progress_bar.value = (timer.time_left/skill.effect_duration) * 100
+
+        print(progress_bar.value, '-', timer.time_left, '-', skill.effect_duration)
+
+        if not skill or skill.effect_remaining_time <= 0:
+                queue_free.call_deferred()
