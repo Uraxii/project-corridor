4b34c05e75366539a39de999cdd3ff07591943ef
uraxii <accounts@brandonpaul.me>
2025-04-06 22:07:02 -0400

    fixed issue with targeting; entities are now highlighted when targeted

--- data/entities/player/player.gd
+++ data/entities/player/player.gd
@@ -1,5 +1,7 @@
 class_name Player extends Entity
 
+const SHADER_HIGHLIGHT: Resource = preload("res://data/materials/highlight.tres")
+
 @export var stats: Stats
 
 @export var player_plate: NamePlate
@@ -9,6 +11,8 @@ class_name Player extends Entity
 @onready var movement: Movement = %Movement
 
 var player_info: PlayerInfo
+
+var target_original_material: Material
 
 var skill_binds: Dictionary = {
         'bar_1_skill_1': 'attack',
@@ -40,7 +44,7 @@ func _process(delta: float) -> void:
         if input.target_next:
                 targeting.target_next()
         if input.target_self:
-                targeting.target_entity(self)
+                targeting.set_target(self)
         if input.cancel:
                 set_target(null)
 
@@ -51,3 +55,28 @@ func _physics_process(delta: float) -> void:
         Server.update_player_info.rpc(body.position, body.rotation)
 
         # player_info = Server.connections[multiplayer.get_unique_id()]
+
+
+func set_target(new_target: Entity) -> void:
+        var target_visuals
+
+        if target != null:
+                target_visuals = target.body.get_children()
+
+                for element in target_visuals:
+                        if element is MeshInstance3D:
+                                element.set_surface_override_material(0, target_original_material)
+
+
+        if new_target == null:
+                return
+
+        target_visuals = new_target.body.get_children()
+
+        for element in target_visuals:
+                if element is MeshInstance3D:
+                        target_original_material = element.get_surface_override_material(0)
+                        element.set_surface_override_material(0, SHADER_HIGHLIGHT)
+
+        target = new_target
+        changed_target.emit(target)
