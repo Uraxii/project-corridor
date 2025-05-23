04cab2c87622184ae034a078135ae6629f57e4d6
uraxii <accounts@brandonpaul.me>
2025-05-23 02:30:20 -0400

    WIP Golang backend API test

---
+++ backend/server/go.mod
@@ -0,0 +1,8 @@
+module server
+
+go 1.24.3
+
+require (
+	github.com/gorilla/websocket v1.5.3
+	google.golang.org/protobuf v1.36.6
+)
---
+++ backend/server/pkg/packets/util.go
@@ -0,0 +1,20 @@
+package packets
+
+type Msg = isPacket_Msg
+
+func NewCredientials(user string, secret string) Msg {
+	return &Packet_Credential{
+		Credential: &CredentialMessage{
+			User: user,
+			Secret: secret,
+		},
+	}
+}
+
+func NewId(id uint64) Msg {
+	return &Packet_Id{
+		Id: &IdMessage{
+			Id: id,
+		},
+	}
+}
